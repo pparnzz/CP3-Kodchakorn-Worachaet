09bb75c0abba8700ec81e125038d4167f47c36cd
pparnzz <65115328+pparnzz@users.noreply.github.com>
2020-08-16 15:51:03 +0700

    Excercise4_Kodchakorn_W.py

---
+++ Exercise4_Kodchakorn_W.py
@@ -0,0 +1,11 @@
+engScr = 82
+busScr = 79.5
+IntComScr = 89
+ComProScr = 87
+
+print("--- Your Score ---")
+print("------------------")
+print("Foundation English :",engScr)
+print("General Business :",busScr)
+print("Introduction to Computer Systems :",IntComScr)
+print("Computer Programming :",ComProScr)
